710ba5307d64cae1aa519347085c3d084673f125
elvinawibisono <66840764+elvinawibisono@users.noreply.github.com>
2023-02-14 17:15:51 -0500

    tabindex

--- project2.js
+++ project2.js
@@ -19,6 +19,31 @@ $(document).ready(function() {
     }
   });
 
+  $("#receivespambutton").keypress(function(event) {
+    
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+      if(keycode == '13'){
+
+        $("#message1").show();
+
+      if($($("#receivespambutton").children()[0]).attr("src")=="pics/unchecked.png") {
+        $($("#receivespambutton").children()[0]).attr("src","pics/checked.png");
+        $("#message3").show();
+        $("#message4").hide();
+        $("#spamyn").val("y");
+        
+      } else {
+        $($("#receivespambutton").children()[0]).attr("src","pics/unchecked.png");
+        $("#message4").show();
+        $("#message3").hide();
+        $("#spamyn").val("n");
+      }
+      }
+  
+
+    
+  });
+
 
   // Play video
   $("#videoplayer").mousedown(function() {
@@ -54,10 +79,45 @@ $(document).ready(function() {
   });
 
 
+/*
+  //Enter
+  $("signupbotton").focus(function(){
+
+    $("#signupbutton").keypress(function(event) {
+      var keycode = (event.keyCode ? event.keyCode : event.which);
+      if(keycode == '13'){
+  
+        if($("#fn").val()=="" || $("#mi").val()=="" || $("#ln").val()=="") {
+          $("#name").addClass("error");
+    
+          return;
+        } else {
+          $("#name").removeClass("error");
+        }
+    
+        if($("#em").val()=="") {
+          $("#email").addClass("error");
+    
+          return;
+        } else {
+          $("#name").removeClass("error");
+        }
+    
+        alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
+  
+      }
+      
+    })
+  
+  });
+  /*
+
+
+
 
 
   // enter
-  /*$("#signupbutton").keyup(function(event) {
+  /*$("#signupbutton").keypress(function(event) {
     if (event.code == 'Enter') {
       if($("#fn").val()=="" || $("#mi").val()=="" || $("#ln").val()=="") {
         $("#name").addClass("error");
